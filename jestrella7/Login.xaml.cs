e2ad0f0551dde32bfec105c63e87d10400aa1bcc
master <master@SMLAB408PC12_23>
2023-01-24 19:30:12 -0500

    Agregar archivos de proyecto.

---
+++ jestrella7/Login.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using System.IO;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace jestrella7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        private SQLiteAsyncConnection connection;
+        public Login()
+        {
+            InitializeComponent();
+            connection = DependencyService.Get<DataBase>().GetConnection();
+        }
+
+        public static IEnumerable<Estudiante> Select_where(SQLiteConnection db, string usuario, string contrasena)
+        {
+            return db.Query<Estudiante>("SELECT * FROM Estudiante where Usuario=? and Contrasena=?", usuario, contrasena);
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+                var db = new SQLiteConnection(ruta);
+                db.CreateTable<Estudiante>();
+                IEnumerable<Estudiante> resultado = Select_where(db, txtUsuario.Text, txtContrasena.Text);
+                if (resultado.Count() > 0)
+                {
+                    Navigation.PushAsync(new ConsultaRegistro());
+                }
+                else
+                {
+                    DisplayAlert("Alerta", "Usuario o Contraseña Incorrectos", "Cerrar");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            } 
+
+        }
+
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
+        }
+    }
+}
---
+++ jestrella7/Registro.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace jestrella7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        private SQLiteAsyncConnection connection;
+        public Registro()
+        {
+            InitializeComponent();
+            connection = DependencyService.Get<DataBase>().GetConnection();
+        }
+
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            var datos = new Estudiante
+            {
+                Nombre = txtNombre.Text, Usuario = txtUsuario.Text, Contrasena = txtContrasena.Text,
+            };
+            connection.InsertAsync(datos);
+            txtNombre.Text = "";
+            txtContrasena.Text = "";
+            txtUsuario.Text = "";
+            Navigation.PushAsync(new Login());
+            
+        }
+    }
+}
